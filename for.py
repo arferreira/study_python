ec1515ba91f4ffaf3409c2aada6040afd2c470ae
Antonio Ricardo <antonio.eschola@gmail.com>
2017-05-02 21:24:12 -0400

    Adding loop conditionals

---
+++ for.py
@@ -0,0 +1,6 @@
+languages = ['Python', 'Ruby', 'C']
+
+for language in languages:
+    print(language)
+
+
---
+++ while.py
@@ -0,0 +1,5 @@
+year = 2017
+
+while year < 2050:
+    print('We are in: ', year)
+    year += 1
